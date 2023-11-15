8d2351bb628463d0fd63b2be0ae4be4e6f3d793d
Mattéo Benhalima <150261713+NG-48@users.noreply.github.com>
2023-11-15 18:47:09 +0100

    Add files via upload

--- SAE 1.01/src/Test.java
+++ SAE 1.01/src/Test.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Test {
@@ -7,9 +9,10 @@ public class Test {
         int[][] trous=new int[9][9];
         SudokuBase.initGrilleIncomplete(Ut.randomMinMax(5,25),sudoku,trous);
         boolean[] tabBool= {false,false,false,true,true,false};
-        System.out.println(SudokuBase.saisirEntierMinMax(1,9));
-        System.out.println(SudokuBase.uneValeur(tabBool));
         SudokuBase.afficheGrille(3,sudoku);
         SudokuBase.afficheGrille(3,trous);
+        System.out.println("");
+        int[][] matrice = new int[25][25];
+        System.out.println(SudokuBase.tourHumain(sudoku,trous));
     }
 }
